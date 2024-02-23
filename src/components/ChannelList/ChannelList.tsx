b2b520e5975fef58802087c00b6985e7243db8b6
srcatt <thesourcecatt@outlook.com>
2024-02-23 18:57:15 +0800

    fix(front): main component position

--- src/components/ChannelList/ChannelList.tsx
+++ src/components/ChannelList/ChannelList.tsx
@@ -1,26 +1,36 @@
-import styles from "./styles.module.scss"
-import {PropsWithChildren, ReactNode} from "react";
-import Link from "next/link";
-import Icon from "@mdi/react";
-import {mdiCast} from "@mdi/js";
+import './globals.scss';
+import styles from './styles.module.scss';
+import { ReactNode } from 'react';
+import Link from 'next/link';
+import Icon from '@mdi/react';
+import { mdiCast } from '@mdi/js';
 
-interface ChannelListProps{
-    hide?:boolean
-    children?:ReactNode
+interface ChannelListProps {
+  hide?: boolean;
+  children?: ReactNode;
 }
 
-export function ChannelList({hide,children}:ChannelListProps) {
-    return <div className={styles.channels} data-hide={hide}>{children}</div>
+export function ChannelList({ hide, children }: ChannelListProps) {
+  return (
+    <div className={styles.channels} data-hide={hide}>
+      {children}
+    </div>
+  );
 }
 
-interface ChannelListItemProps{
-    target:string,
-    watch?:boolean
+interface ChannelListItemProps {
+  target: string;
+  watch?: boolean;
 }
 
-export function ChannelListItem({target,watch=false}:ChannelListItemProps) {
-    return <Link className={styles.channel} href={"/"+target} data-watch={watch}>
-        <Icon path={mdiCast}/>
-        <span>{target}</span>
+export function ChannelListItem({
+  target,
+  watch = false,
+}: ChannelListItemProps) {
+  return (
+    <Link className={styles.channel} href={'/' + target} data-watch={watch}>
+      <Icon path={mdiCast} />
+      <span>{target}</span>
     </Link>
+  );
 }
